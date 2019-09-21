a58a8202815d0d01cc4f97e3518a319a9fa744ec
Pando85 <pando855@gmail.com>
2019-09-21 12:47:15 +0200

    Refactor netdata load_screts.sh to avoid overwrite config file

--- apps/6-netdata/load_secrets.sh
+++ apps/6-netdata/load_secrets.sh
@@ -1,7 +1,8 @@
 #!/bin/bash
 set -e
+TMP_FILE=$(mktemp /tmp/file.XXXXXX)
 TELEGRAM_BOT_TOKEN=$(pass kubernetes.grigri/netdata/telegram_bot_token);
-sed "s/__TELEGRAM_BOT_TOKEN__/$TELEGRAM_BOT_TOKEN/g" -i 3_config.yml
+sed "s/__TELEGRAM_BOT_TOKEN__/$TELEGRAM_BOT_TOKEN/g" 3_config.yml.template > $TMP_FILE
 NETDATA_API_KEY=$(pass kubernetes.grigri/netdata/netdata_api_key);
-sed "s/__NETDATA_API_KEY__/$NETDATA_API_KEY/g" -i 3_config.yml
+sed "s/__NETDATA_API_KEY__/$NETDATA_API_KEY/g" $TMP_FILE > 3_config.yml
 
