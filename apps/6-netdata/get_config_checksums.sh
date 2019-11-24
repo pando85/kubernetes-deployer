bf1f723287abc984c079543ce385cd0a322731e2
Pando85 <pando855@gmail.com>
2019-11-24 19:00:17 +0100

    Add monitoring to app and remove netdata

--- apps/6-netdata/get_config_checksums.sh
+++
@@ -1,3 +0,0 @@
-#!/bin/bash
-echo slave checksum: $(kubectl get -n netdata configmap/netdata-conf-slave -o yaml | sha256sum)
-echo master checksum: $(kubectl get -n netdata configmap/netdata-conf-master -o yaml | sha256sum)
--- apps/6-netdata/load_secrets.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-set -e
-TMP_FILE=$(mktemp /tmp/file.XXXXXX)
-TELEGRAM_BOT_TOKEN=$(pass kubernetes.grigri/telegram_bot_token);
-sed "s/__TELEGRAM_BOT_TOKEN__/$TELEGRAM_BOT_TOKEN/g" 3_config.yml.template > $TMP_FILE
-NETDATA_API_KEY=$(pass kubernetes.grigri/netdata/netdata_api_key);
-sed "s/__NETDATA_API_KEY__/$NETDATA_API_KEY/g" $TMP_FILE > 3_config.yml
-
