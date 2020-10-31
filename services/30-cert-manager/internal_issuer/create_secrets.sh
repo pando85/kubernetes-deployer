075ead49987ee43b90bc7e7fcf9620c69060d129
Pando85 <pando855@gmail.com>
2020-10-31 18:43:29 +0100

    cert-manager: Change to helmfile

--- services/30-cert-manager/internal_issuer/create_secrets.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-pass kubernetes.grigri/ca/key | base64 -d > /tmp/tls.key
-pass kubernetes.grigri/ca/crt | base64 -d > /tmp/tls.crt
-
-kubectl create secret tls kubernetes-internal-ca-key-pair \
-   --cert=/tmp/tls.crt \
-   --key=/tmp/tls.key \
-   --namespace=cert-manager
